98814c4483282ae30aeb3321e669da8742ce5ffa
Hotsse <dkdlrja@naver.com>
2020-06-12 01:02:51 +0900

    테스트 데이터 개선

--- src/test/java/me/hotsse/visionapi/JavaRegExTests.java
+++ src/test/java/me/hotsse/visionapi/JavaRegExTests.java
@@ -12,7 +12,13 @@ class JavaRegExTests {
 	@Test
 	void contextLoads() {
 		
-		final String originText = "키스체크KIS정보통신가맹점덤/주소가실제와다른경우신고한내(포상급10만원지음금)여산금용협회(02-2011-0777)IBK비씨제크승인TID:11625247-0814-일시불[신용승인]거래일시20/05/3120:00:08[회원용]6210-03**-****-0255금액:15,909원부가세:1,591원합계:17,500원승인번호(비씨카드BCESC)가맹점/사업자김은진(TRL:0284899?)서울특별시영등포구대림로731층(대림동)779127510C]670/218-04-77660신수폭발50,000원이하는무서명개래입니다알림EDC매출전표VANKEY:0152012720082980G8044958-0814-SR67[00021";
+		final String originText = "가까운행복을만나다GSZS0283596741023062895GS25대림밸리소홈점김승욱서울영등포구대림동1101-7번지2020/06/11박*욱NO:14003※정부방침에의해교환/환불은반드시영수증을지참하셔야하며,카드결제는30일(07월11일)이내카드와영수종지참시가능합니다미닛오렌지350김치찌개(컵밥)톡톡김지알밥합계수량/금액판촉/팝할인1,950증정품7,0008,950-450과세매출부가세계7,7287728,5008,500합신용카드*-신용카드전표(고객용)카드번호5365-10**--3707사용금액8,500원할부0(매입사:국민카드)송인번호22045572--20/06/1120:13:19----**2020년GS25_30주년진심으로고맙습니다.11244";
+		
+		System.out.println("Google Vision API");
+		System.out.println("---");
+		
+		System.out.println("OCR 분석 텍스트 원본");
+		System.out.println(originText);
 		
 		System.out.println("---");
 		
@@ -27,7 +33,7 @@ class JavaRegExTests {
 		System.out.println("---");
 		
 		// 금액 관련 정보 획득
-		List<String> cashInfoText = this.fetchStringWithRegEx(originText, "[0-9||,]{1,}원");
+		List<String> cashInfoText = this.fetchStringWithRegEx(originText, "[0-9||,]{3,}원");
 		
 		System.out.println("금액 관련 수집정보");
 		for(String text : cashInfoText) {
